781a8a25277b216b0a2d1d19d3adc9d6e09feab0
Reshma Jabeen <31253728+Rjabeen@users.noreply.github.com>
2018-02-15 19:57:16 -0600

    manipulated Dom to show errors in js and changed content of paragraph in html n changed h1 font size

--- site.js
+++ site.js
@@ -2,19 +2,56 @@
 $('html').removeClass('no-js').addClass('js');
 $('#contactInfo-form').on('submit' ,
 function(e) {
+
+  e.preventDefault();
+
+  while($('#problem').length != 0)
+  {
+    $('#problem').remove();
+  }
+
+
    var email = $('#email').val();
-   var valid = /.+@.+\..+/g;
-   var validemail = valid.test(email);
+   var phone = $('#phone').val();
    var name = $('#name').val();
-   e.preventDefault();
+
+   var validE = /.+@.+\..+/g
+   var validP = /.+\d.+/g
+
+   var validemail = validE.test(email);
+   var validphone = validP.test(phone);
+   var validbirthday = false;
+
+
+
    if(!validemail){
      console.log('Invalid email');
-     alert("The email you entered was invalid");
-     return false;
+     // alert("The email you entered was invalid");
+
+     $('#email').parent().append("<p id='problem'>Email is invalid!</p>")
+
    }
+
+   if(!validphone){
+     console.log('Invalid phone');
+     // alert("The phone # you entered was invalid");
+
+     $('#phone').parent().append("<p id='problem'>phone number is invalid!</p>")
+
+   }
+
+   if(!validbirthday){
+     console.log('Invalid birthday');
+     // alert("The phone # you entered was invalid");
+
+     $('#birthday').parent().append("<p id='problem'>birthday is invalid!</p>")
+
+   }
+
+
    else {
       console.log('Valid email');
-      $('body').replaceWith('<h1 id="submission"> Thank you ' + name + '!  your sign up has been completed.</h1>');
+      $('body').replaceWith('<h1 id="submission"> Thank you ' + name + '  for signing up .</h1>');
     }
 
 
